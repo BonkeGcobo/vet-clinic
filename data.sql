23d8b6e15a2ef8fce5214f74783a3bf7e38c2e09
BonkeG <madixmicro@gmail.com>
2022-04-27 13:35:25 +0200

    remove unused code

--- data.sql
+++ data.sql
@@ -1,8 +1,4 @@
-/* Populate database with sample data. */
 INSERT INTO animals(name, date_of_birth, escape_attemps,neutered, weight_kg) values ('Agumon', '2020-02-03',0 ,TRUE, 10.23);
 INSERT INTO animals(name, date_of_birth,escape_attemps ,neutered, weight_kg) values ('Gabumon', '2018-11-15',2, TRUE, 8);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attemps) values('Pikachu', '2021-01-07',15.04, FALSE, 1);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attemps) values ('Devimon', '2021-05-12',11, TRUE, 5);
-/*INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');*/
--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
-/*Queries that provide answers to the questions from all projects.*/
 SELECT * FROM animals where name LIKE '%mon';
 SELECT * FROM animals where date_of_birth BETWEEN '2016-01-01' and '2019-12-31';
 SELECT * FROM animals where neutered=TRUE AND escape_attemps <3;
